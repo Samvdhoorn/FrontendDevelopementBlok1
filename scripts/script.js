7b0be96a30f2adf954d2b764f3e2f164e1344ba9
Samvdhoorn <sam.van.der.hoorn@hva.nl>
2024-09-17 13:23:41 +0200

    Begin carousel

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("hi");
+
+// let currentIndex = 0;
+// const images = document.querySelector('section:nth-of-type(3) li img');
+// const totalImages = images.length;
+
+// function showNextImage() {
+//     currentIndex++; 
+//         if (currentIndex >= totalImages) {
+//             currentIndex = 1;
+//         }
+
+//         const carousel = document.querySelector('section:nth-of-type(3) li')
+//         const offset = -currentIndex * (images[0].offsetWidth + 10);
+//         carousel.style.transform = `translateX(${offset}px)`;
+
+//     }
+    
+//     setInterval(showNextImage, 500);
+
